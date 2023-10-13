f013866a8caa5d1bca8a4afed0842d674760a041
wajones98 <will.jones@byzgen.com>
2023-10-13 17:02:15 +0200

    remove lazygit config from lazygit plugins dir

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,11 +1,3 @@
--- [[ Configure LazyGit ]]
-
-vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', {
-  silent = true,
-  desc = 'Open LazyGit window'
-})
-
-
 return {
       "kdheepak/lazygit.nvim",
       -- optional for floating window border decoration
