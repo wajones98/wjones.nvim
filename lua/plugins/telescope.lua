088d532f67bc1f1fa8af949bd7c264c098ddb4c6
wajones98 <will.jones@byzgen.com>
2024-02-02 13:42:41 +0000

    Groups and descriptions for telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,8 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' }, 
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[F]iles" })
+        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[G]rep" })
+        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[D]iagnostics" })
     end
 }
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    config = function()
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>s"] = {
+                name = "[S]earch"
+            }
+        })
+    end
+} 
