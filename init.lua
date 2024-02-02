ac1470dde2c80b737dbace678bb847b0d79299f5
wajones98 <will.jones@byzgen.com>
2024-02-02 13:18:09 +0000

    Seperate init.lua into plugins/ and vim-options

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -16,27 +17,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { 'nvim-lua/plenary.nvim' } },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
+require("vim-options")
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = { "lua", "javascript" },
-    highlight = { enable = true },
-    indent = { enable = true } 
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
