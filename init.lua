7c15fcb298319a25eabde773d84d2c2b63ddf0fb
wajones98 <will.jones@byzgen.com>
2024-02-02 13:18:53 +0000

    Move vim commands to vim-options.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
