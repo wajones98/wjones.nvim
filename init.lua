3a56b7e18c15e784d68b30ce74c04d6e4bbc3dc1
wajones98 <will.jones@byzgen.com>
2024-02-02 09:20:47 +0000

    Add Treesitter and Telescope

--- init.lua
+++ init.lua
@@ -16,7 +16,20 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { 'nvim-lua/plenary.nvim' } },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
