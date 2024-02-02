18c30f827b420ff2b1278220dcd399890b39366e
wajones98 <will.jones@byzgen.com>
2024-02-02 15:06:08 +0000

    Setup completions and LSPConfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,11 +9,25 @@ return {
         "williamboman/mason-lspconfig.nvim",
         opts = {
             auto_install = true,
+            ensure_installed = { "lua_ls" },
         },
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require("lspconfig")
+            lspconfig.tsserver.setup({
+                capabilities = capabilities
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+
             vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Lsp Hover" })
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[D]efinition" })
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "[R]eferences" })
