3041645d9d2686cd7b4e421783c44dc42125bb30
wajones98 <will.jones@byzgen.com>
2024-02-02 15:26:48 +0000

    filter formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -14,7 +14,13 @@ return {
                         group = augroup,
                         buffer = bufnr,
                         callback = function()
-                            vim.lsp.buf.format({ async = false })
+                            vim.lsp.buf.format({
+                                async = false,
+                                bufnr = bufnr,
+                                filter = function(client)
+                                    return client.name == "null-ls"
+                                end,
+                            })
                         end,
                     })
                 end
