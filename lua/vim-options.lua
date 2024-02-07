12cf4919c74ad02177ba6122d2f5ad16fac9ce54
wajones98 <will.jones@byzgen.com>
2024-02-07 08:10:55 +0000

    Update vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,4 +6,13 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
+
+-- Indent wrapped lines
+vim.o.breakindent = true
+
+-- Show autocomplete menu but require manual selection
+vim.o.completeopt = "menuone,noselect"
+
+-- Enable 24-bit colors
+vim.o.termguicolors = true
