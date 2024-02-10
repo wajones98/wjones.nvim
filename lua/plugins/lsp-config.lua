88f73949edd6df789f1f1e4bfa6044fc0becfd1a
Will <w.a.jones1998@gmail.com>
2024-02-10 23:20:40 +0000

    Add templ to TS and format on save

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,11 +18,37 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+
+			vim.filetype.add({ extension = { templ = "templ" } })
+			lspconfig.templ.setup({
+				capabilities = capabilities,
+			})
+			local templ_format = function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				local filename = vim.api.nvim_buf_get_name(bufnr)
+				local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+				vim.fn.jobstart(cmd, {
+					on_exit = function()
+						-- Reload the buffer only if it's still the current buffer
+						if vim.api.nvim_get_current_buf() == bufnr then
+							vim.cmd("e!")
+						end
+					end,
+				})
+			end
+			vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
+
 			lspconfig.tsserver.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.html.setup({
 				capabilities = capabilities,
+				filetypes = { "html", "templ" },
+			})
+			lspconfig.htmx.setup({
+				capabilities = capabilities,
+				filetypes = { "html", "templ" },
 			})
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
@@ -31,12 +57,6 @@ return {
 				capabilities = capabilities,
 			})
 			lspconfig.terraformls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.htmx.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.templ.setup({
 				capabilities = capabilities,
 			})
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript", "go", "terraform" },
+			ensure_installed = { "lua", "javascript", "go", "terraform", "templ" },
 			highlight = { enable = true },
 			indent = { enable = true },
 			auto_install = true,
