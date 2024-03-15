b136281ca626c90b85025e299175f466269a1c33
Will <w.a.jones1998@gmail.com>
2024-03-15 18:42:59 +0000

    Format files and add lazygit

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,23 +1,23 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 vim.opt.number = true
 -- TODO: Toggle keybinding for relative numbers
 vim.opt.relativenumber = true
 
-vim.opt.mouse = 'a' 
+vim.opt.mouse = "a"
 
 vim.opt.showmode = false
 
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.breakindent = true
 
 vim.opt.undofile = true
 
-vim.opt.signcolumn='number'
+vim.opt.signcolumn = "number"
 
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
@@ -28,10 +28,10 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
