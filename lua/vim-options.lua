63575e021876cbb99589be3ff51018d914375143
Will <w.a.jones1998@gmail.com>
2024-07-21 20:54:12 +0100

    Remove M characters

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -37,3 +37,20 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Function to remove ^M characters
+local function remove_carriage_returns()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  for i, line in ipairs(lines) do
+    lines[i] = line:gsub("\r", "")
+  end
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
+end
+
+-- Autocommand to run the function on paste
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.schedule(remove_carriage_returns)
+  end,
+})
